f5d17aa3d717e6b83ec0ef8a59aab78a1ece7f19
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-07-27 15:10:30 +0200

    feat: HeroCard to show hero detail

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher';
-
+import { HeroCard } from './HeroCard';
+HeroCard;
 export const HeroList = ({ publisher }) => {
   const heroesList = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className='row row-cols-1 row-cols-md-3 g-3'>
       {heroesList.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
