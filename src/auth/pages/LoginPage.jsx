124d6026d8123bdc593489a1d1b08890c7d62f0e
Javier Sánchez <javier.sanchezortiz@xploregroup.es>
2022-07-27 12:37:32 +0200

    feat: Add navigation buttons

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/', {
+      replace: true,
+    });
+  };
+
   return (
     <>
-      <h1>LoginPage</h1>
+      <div className='container mt-5'>
+        <h1>LoginPage</h1>
+        <hr />
+        <button className='btn btn-primary' onClick={handleLogin}>
+          Login
+        </button>
+      </div>
     </>
   );
 };
